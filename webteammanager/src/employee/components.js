44268a5f3902d8909b91209a12aef452a9e7296a
kczan <fchrzan@gmail.com>
2020-09-14 19:10:29 +0200

    Added sorting emps feature

--- webteammanager/src/employee/components.js
+++ webteammanager/src/employee/components.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
 import { apiGetEmployeeList } from "./lookup";
 import { Employee } from "./detail";
+import { sortObjectsArray } from "./utilities";
 
 export function EmployeesListComponent(props) {
   const [employeesInit, setEmployeesInit] = useState([]);
   const [employees, setEmployees] = useState([]);
   const [nextUrl, setNextUrl] = useState(null);
   const [employeesDidSet, setEmployeesDidSet] = useState(false);
+  const [currentOption, setCurrentOption] = useState({ value: "az" });
 
   let department = "";
   if (props.department) {
@@ -52,8 +57,35 @@ export function EmployeesListComponent(props) {
     }
   };
 
+  const options = [
+    {
+      type: "group",
+      name: "Last name",
+      items: [
+        { value: "az", label: "A-Z" },
+        { value: "za", label: "Z-A" },
+      ],
+    },
+    {
+      type: "group",
+      name: "Salary",
+      items: [
+        { value: "asc", label: "Ascending" },
+        { value: "desc", label: "Descending" },
+      ],
+    },
+  ];
+
+  sortEmployees(currentOption.value, employees);
+
   return (
     <React.Fragment>
+      <Dropdown
+        className="sort-dropdown"
+        options={options}
+        onChange={setCurrentOption}
+        placeholder="Sort by:"
+      />
       <div className="d-flex flex-wrap ml-3 my-5">
         {employees.map((employee) => {
           return (
@@ -73,4 +105,24 @@ export function EmployeesListComponent(props) {
       )}
     </React.Fragment>
   );
+}
+
+function sortEmployees(sortOption, employeesArray) {
+  switch (sortOption) {
+    case "az":
+      employeesArray.sort(sortObjectsArray("last_name"));
+      break;
+    case "za":
+      employeesArray.sort(sortObjectsArray("-last_name"));
+      break;
+    case "asc":
+      employeesArray.sort(sortObjectsArray("salary", true));
+      break;
+    case "desc":
+      employeesArray.sort(sortObjectsArray("-salary", true));
+      break;
+    default:
+      employeesArray("last_name");
+  }
+  return employeesArray;
 }
---
+++ webteammanager/src/employee/utilities.js
@@ -0,0 +1,16 @@
+export function sortObjectsArray(property, numeric = false) {
+  let sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    if (numeric) {
+      a[property] = Number(a[property]);
+      b[property] = Number(b[property]);
+    }
+    var result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * sortOrder;
+  };
+}
